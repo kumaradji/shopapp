0ee5dc924b4cdb83d35d0a94b42d56adbb8a6eff
Name <email@example.ru>
2023-03-27 19:24:56 +0300

    продолжаем SF

--- simpleapp/views.py
+++ simpleapp/views.py
@@ -1,5 +1,7 @@
 # Импортируем класс, который говорит нам о том,
 # что в этом представлении мы будем выводить список объектов из БД
+from datetime import datetime
+
 from django.views.generic import ListView, DetailView
 from .models import Product
 
@@ -9,6 +11,12 @@ class ProductsList(ListView):
     ordering = 'name'
     template_name = 'products.html'
     context_object_name = 'products'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['time_now'] = datetime.utcnow()
+        context['next_sale'] = "Распродажа в среду!"
+        return context
 
 
 class ProductDetail(DetailView):
