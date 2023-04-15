26992d0312c7c3b2e915a10adb20cbf0b7b6b5dd
kumaradji <kumaradji@me.com>
2023-04-15 13:27:43 +0300

    создана письмо для нового пользователя при регистрации

--- accounts/forms.py
+++ accounts/forms.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 
 from allauth.account.forms import SignupForm
 from django.contrib.auth.models import Group
+from django.core.mail import send_mail
 
 
 class CustomSignupForm(SignupForm):
@@ -11,21 +12,11 @@ class CustomSignupForm(SignupForm):
         user = super().save(request)
         common_users = Group.objects.get(name="common users")
         user.groups.add(common_users)
+        send_mail(
+            subject='Добро пожаловать в наш интернет-магазин!',
+            message=f'{user.username}, вы успешно зарегистрировались!',
+            from_email=None,  # будет использовано значение DEFAULT_FROM_EMAIL
+            recipient_list=[user.email],
+        )
         return user
 
-
-class SignUpForm(UserCreationForm):
-    email = forms.EmailField(label="Email")
-    first_name = forms.CharField(label="Имя")
-    last_name = forms.CharField(label="Фамилия")
-
-    class Meta:
-        model = User
-        fields = (
-            "username",
-            "first_name",
-            "last_name",
-            "email",
-            "password1",
-            "password2",
-        )
--- shopapp/settings.py
+++ shopapp/settings.py
@@ -2,9 +2,7 @@ import os.path
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 SECRET_KEY = 'django-insecure-6qzy&+*o&&(+#re6mm6)gn9jc5mgll-dyum4gli3b(&smu-v3l'
-
 DEBUG = True
 ACCOUNT_FORMS = {"signup": "accounts.forms.CustomSignupForm"}
 ALLOWED_HOSTS = []
@@ -19,7 +17,6 @@ INSTALLED_APPS = [
     'django.contrib.sites',
     'django.contrib.flatpages',
     'django_filters',
-
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
@@ -32,6 +29,17 @@ ACCOUNT_UNIQUE_EMAIL = True
 ACCOUNT_USERNAME_REQUIRED = False
 ACCOUNT_AUTHENTICATION_METHOD = 'email'
 ACCOUNT_EMAIL_VERIFICATION = 'none'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_HOST = 'smtp.yandex.ru'
+EMAIL_PORT = 465
+EMAIL_HOST_USER = "kumar9951447@yandex.ru"
+EMAIL_HOST_PASSWORD = "bjgmjgnocpdoypkf"
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True
+
+DEFAULT_FROM_EMAIL = "kumar9951447@yandex.ru"
 
 SITE_ID = 1
 
@@ -95,15 +103,10 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 LANGUAGE_CODE = 'ru'
-
 TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
 USE_TZ = True
-
 STATIC_URL = 'static/'
-
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATICFILES_DIRS = [
